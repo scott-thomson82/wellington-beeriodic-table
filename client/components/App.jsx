29426959eb5b494ba80575bb366cb71766607582
Scott Thomson <scottthomson_57@hotmail.com>
2020-02-02 14:45:36 +1300

    mapping over beersdata array

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,24 +2,20 @@ import React from 'react'
 import Header from './Header'
 import Element from './Element'
 
-import beersData from '..data/beersData'
+
+
+import beersData from '../data/beersData'
 
 
 
 const App = () => {
-
+  const beerComponents = beersData.map(beer => <Element number={beer.number} beerStyle={beer.beerStyle} symbol={beer.symbol} beerColor={beer.color}/>)
   
-    
-    const beerComponents = beersData.map((beers => <Element number={beers.number} beerColor={beers.beerColor} beerStyle={beers.beerStyle} symbol={beers.symbol} />)
-      )
-
-  
-
-
-return (
+  return (
     <React.Fragment>
     <Header />
     {beerComponents}
+    
     </React.Fragment>
   )
 }
